944e554f6dbf7845e1e7e22fea36472c0693b019
Jeff Heisz <jmheisz@gmail.com>
2021-05-04 03:05:41 +0000

    Added home page with in-progress/new display support

--- pages/home.php
+++ pages/home.php
@@ -12,13 +12,61 @@ set_include_path(dirname(__DIR__));
 
 /* Start with the common initialization elements */
 include_once('src/inc/init.inc');
+include_once('src/inc/header.inc');
 
-/* IN PROGRESS BADGES */
+/* Welcome, welcome, welcome! */
+if ($User->badgeCount == 0) {
+    echo '<div class="home-welcome-header">' .
+           '<div><b>Welcome ' . $User->name . '!</b></div>' . "\n" .
+           '<div> You look new around here, ' .
+                 'might I suggest starting with the ' .
+                 '<a href="/module/pathways-basic">Pathways Basics</a> ' .
+                 'module?' .
+           '</div>' . "\n";
+         '</div>' . "\n";
+}
 
-/* IN PROGRESS TRAILS */
+echo '<div class="home-content">' . "\n";
 
-/* FAVOURITES */
+/* In-progress modules */
+$ipMods = $User->inProgressModules(3);
+if (count($ipMods) != 0) {
+    echo '<div class="home-header">In-Progress Modules</div>' . "\n" .
+         '<div class="home-summary-row">' . "\n";
+    foreach($ipMods as $mod) {
+        echo '<a class="pmcell-link" href="/module/' . $mod['id'] . '">' . "\n" .
+                Pathways\Utility::FormatPathModCell($mod) . "\n" .
+             '</a>' . "\n";
+    }
+    echo '</div>' . "\n";
+}
 
-/* NEWEST */
+/* In-progress paths */
+$ipPaths = $User->inProgressPaths(3);
+if (count($ipPaths) != 0) {
+    echo '<div class="home-header">In-Progress Paths</div>' . "\n" .
+         '<div class="home-summary-row">' . "\n";
+    foreach($ipPaths as $pth) {
+        echo '<a class="pmcell-link" href="/path/' . $pth['id'] . '">' . "\n" .
+                Pathways\Utility::FormatPathModCell($pth) . "\n" .
+             '</a>' . "\n";
+    }
+    echo '</div>' . "\n";
+}
+
+/* Newest unworked modules */
+$newMods = $User->newestModules(3);
+if (count($newMods) != 0) {
+    echo '<div class="home-header">Newest Modules</div>' . "\n" .
+         '<div class="home-summary-row">' . "\n";
+    foreach($newMods as $mod) {
+        echo '<a class="pmcell-link" href="/module/' . $mod['id'] . '">' . "\n" .
+                Pathways\Utility::FormatPathModCell($mod) . "\n" .
+             '</a>' . "\n";
+    }
+    echo '</div>' . "\n";
+}
+
+echo '</div>' . "\n";
 
 ?>
